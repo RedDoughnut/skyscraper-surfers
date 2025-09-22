683420ec63e22077102436cb4889fc648c9090ea
David Tomić <davidtomic10119@gmail.com>
2025-09-23 00:08:54 +0200

    added title screen function, score and high score

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def main():
     height = engine.height
     fps = 60
     font = pygame.font.Font('assets/8-bit-font.ttf', 30)
+    small_font = pygame.font.Font('assets/8-bit-font.ttf', 18)
     window = pygame.display.set_mode((width, height))
 
     #Title Screen
@@ -18,8 +19,10 @@ def main():
     ALL_KEYS_OFF = tuple([0] * len(pygame.key.get_pressed()))
     selectedOption = 0
     title_image = pygame.image.load("assets/title.png").convert_alpha()
+    pointer = pygame.image.load("assets/Pointer.png").convert_alpha()
     # Player
-    
+    score = 0
+    highscore = 0
     player_x = 150
     player_y = 0
     player_z = -50
@@ -37,25 +40,42 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         buttons = pygame.key.get_pressed()
+        start_frametime = time.time()
         if onTitleScreen:
-            start_frametime = time.time()
             window.fill((0,0,0))
-
             player_x = player_x + dx; player_y = player_y + dy; player_z = player_z + dz
 
             framebuffer = engine.draw3D(player_x, player_y, player_z, player_a, player_l, False)
             pygame.surfarray.blit_array(window, framebuffer)
 
-
+            buttons = pygame.key.get_pressed()
+            if buttons[pygame.K_DOWN] and selectedOption<1:
+                selectedOption+=1
+            if buttons[pygame.K_UP] and selectedOption>0:
+                selectedOption-=1
+            if buttons[pygame.K_RETURN] or buttons[pygame.K_SPACE]:
+                match selectedOption:
+                    case 0:
+                        onTitleScreen = False
+                    case _:
+                        pass
+            hiscore_text = font.render(f"HI: {highscore}", True, (255,0,0))
+            start_text = font.render("START", True, (255,0,0))
+            help_text = font.render("HELP", True, (255,0,0))
+            window.blit(hiscore_text, (window.get_width()//2 - hiscore_text.get_width()//2, 150))
+            window.blit(pointer, (500, 207+50*selectedOption))
+            window.blit(start_text, (window.get_width()//2 - start_text.get_width()//2, 200))
+            window.blit(help_text, (window.get_width()//2 - help_text.get_width()//2, 250))
             window.blit(title_image, ((window.get_width() - title_image.get_width()) // 2, 5))
 
             pygame.display.flip()
             time_passed = time.time() - start_frametime
             pygame.time.delay(int(1000 / fps - time_passed))
-            if buttons[pygame.K_0]:
-                onTitleScreen = False
             continue
-        start_frametime = time.time()
+
+        score += 1
+        if score>highscore:
+            highscore = score
         # player_a, player_l = playerMovement(player_a, player_l)
         dx, dy, dz, player_a, player_l = cameraMovement(player_a, player_l, buttons, player_forward_speed, sensitivity)
         
@@ -64,9 +84,15 @@ def main():
         window.fill((0, 0, 0))
         framebuffer = engine.draw3D(player_x, player_y, player_z, player_a, player_l, True)
         pygame.surfarray.blit_array(window, framebuffer)
-    
+        score_text = small_font.render(f"{score}", True, (255,0,0))
+        hiscore_text = small_font.render(f"HI: {highscore}", True, (255,0,0))
+        window.blit(score_text, (window.get_width()//2 - score_text.get_width()//2, 5))
+        window.blit(hiscore_text, (window.get_width() - hiscore_text.get_width() - 5, 5))
         pygame.display.flip()
 
+        if buttons[pygame.K_ESCAPE]:
+            onTitleScreen = True
+            score = 0
         time_passed = time.time() - start_frametime
         pygame.time.delay(int(1000 / fps - time_passed))
 
