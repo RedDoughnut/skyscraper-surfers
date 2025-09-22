bb71d877c4c65563b47b943298899f823ae86e78
David Tomić <davidtomic10119@gmail.com>
2025-09-22 18:41:52 +0200

    added randomized building location

--- engine.py
+++ engine.py
@@ -111,12 +111,12 @@ def updateMap(player_x, player_y, player_z, player_a):
         if S[s].sector_index == 0:
             for w in range(S[s].wall_start, S[s].wall_end):
                 if W[w].y1 < player_y - 100 or W[w].y2 < player_y - 100:
-                    r = randint(0,400)
+                    r = randint(-100,100)
                     for walls in range(S[s].wall_start, S[s].wall_end):
                         W[walls].y1 += 1200
                         W[walls].y2 += 1200
-                        W[walls].x1 = r
-                        W[walls].x2 = r
+                        W[walls].x1 += r
+                        W[walls].x2 += r
         if S[s].sector_index == 1:
             for w in range(S[s].wall_start, S[s].wall_end):
                 playerMovement()
